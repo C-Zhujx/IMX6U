63d7ab4c15d83220493433a6a37cf7fc04e27ad1
zhujx <17629212869@163.com>
2020-08-07 09:15:02 +0800

    add 12_highperci_delay

---
+++ driver/12_highperci_delay/project/main.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include "bsp_clk.h"
+#include "bsp_led.h"
+#include "bsp_delay.h"
+#include "bsp_int.h"
+#include "bsp_exti.h"
+#include "bsp_key.h"
+#include "bsp_beep.h"
+#include "bsp_epit.h"
+#include "bsp_keyfilter.h"
+
+int main(void)
+{
+	static unsigned char led_state = OFF;
+
+	int_init();				/* 初始化中断 */
+	imx6u_clkinit();		/* 初始化系统时钟 */
+	delay_init();			/* 初始化延时函数 */
+	clk_enable();   		/* 使能外设时钟 */
+    led_init();     		/* 初始化LED */
+	beep_init();			/* 初始化蜂鸣器 */ 
+	keyfilter_init();		/* 初始化按键过滤 */
+
+
+    
+    /* 设置LED闪烁 */
+    while(1) 
+	{
+//		led_state = !led_state;
+//		led_switch(LED0, led_state);
+		delay_ms(1000);
+    }
+    return 0;
+}
